15a773b9c021671150a54f3419fa357491042915
Boyan Mihovski <bmihovski@live.com>
2024-06-24 18:04:06 +0300

    add venv branch

--- nvim/lua/pluginconfigs/dap.lua
+++ nvim/lua/pluginconfigs/dap.lua
@@ -134,6 +134,7 @@ return {
 		"linux-cultist/venv-selector.nvim",
 		ft = "python",
 		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+		branch = "regexp", -- This is the regexp branch, use this for the new version
 		opts = {
 			-- Your options go here
 			-- name = "venv",
